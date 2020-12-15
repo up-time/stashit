3d99d5c33814304880be08b15b390071cc2a5421
shraven <shraven@hotmail.com>
2020-12-15 15:06:50 -0700

    ssh2

--- startup.sh
+++ startup.sh
@@ -4,4 +4,6 @@ echo $SSH_PUBLIC_KEY > ~/.ssh/authorized_keys
 #touch /efs_persistence/itworked.test.log || :
 #date > /efs_persistence/itworked.test.log
 #echo /efs_persistence/itworked.test.log
+mkdir -p /var/run/sshd
+/usr/sbin/sshd -D &
 python app/app.py
