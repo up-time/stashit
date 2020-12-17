78ffb0c6ee1cb17f1621d9671b7a1b966486400c
up-time <50923926+up-time@users.noreply.github.com>
2020-12-17 15:03:33 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 echo $SSH_PUBLIC_KEY
-mkdir -p ~/.ssh/authorized_keys || :
-mkdir -p /var/run/sshd || :
+rm -rf ~/.ssh/authorized_keys || :
+touch ~/.ssh/authorized_keys
 echo $SSH_PUBLIC_KEY > ~/.ssh/authorized_keys
+
+mkdir -p /var/run/sshd || :
 /usr/sbin/sshd -D &
-pwd && ls
+
 python app/app.py
