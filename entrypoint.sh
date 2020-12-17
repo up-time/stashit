89686012f69a2392617a24c6b781b0d771b68641
up-time <50923926+up-time@users.noreply.github.com>
2020-12-17 14:56:30 -0700

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo $SSH_PUBLIC_KEY
+mkdir -p ~/.ssh/authorized_keys || :
+mkdir -p /var/run/sshd || :
+echo $SSH_PUBLIC_KEY > ~/.ssh/authorized_keys
+/usr/sbin/sshd -D &
+pwd && ls
+python app/app.py
