4d312d5a798395214856d7ec1affb777aaeecec7
up-time <50923926+up-time@users.noreply.github.com>
2020-12-17 14:51:28 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 echo $SSH_PUBLIC_KEY
-mkdir -p ~/.ssh/authorized_keys || :
+echo "1"
+rm -rf ~/.ssh/authorized_keys || :
+echo "2"
+touch ~/.ssh/authorized_keys || :
+echo "3"
 echo $SSH_PUBLIC_KEY > ~/.ssh/authorized_keys
-
+echo "4"
 mkdir -p /var/run/sshd || :
+echo "5"
 /usr/sbin/sshd -D &
-pwd && ls app
+echo "6"
 python app/app.py
