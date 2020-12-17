2094e464d58f4d6fdfe66e69aa77daf679bb6d9a
up-time <50923926+up-time@users.noreply.github.com>
2020-12-17 14:10:42 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
+mkdir -p ~/.ssh/authorized_keys
 echo $SSH_PUBLIC_KEY > ~/.ssh/authorized_keys
-## efs_persistence test
-#touch /efs_persistence/itworked.test.log || :
-#date > /efs_persistence/itworked.test.log
-#echo /efs_persistence/itworked.test.log
 mkdir -p /var/run/sshd
 /usr/sbin/sshd -D &
 python app/app.py
