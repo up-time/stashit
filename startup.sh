1b6621131ea1be15962a5a232ac05c0d58883326
up-time <50923926+up-time@users.noreply.github.com>
2020-12-19 15:16:46 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 echo $SSH_PUBLIC_KEY
-echo "1"
 rm -rf ~/.ssh/authorized_keys || :
-echo "2"
 touch ~/.ssh/authorized_keys || :
-echo "3"
 echo $SSH_PUBLIC_KEY > ~/.ssh/authorized_keys
-echo "4"
 mkdir -p /var/run/sshd || :
-echo "5"
 /usr/sbin/sshd -D &
-echo "6"
+##persistence testing
+##persistence testing
+##persistence testing
+touch /usr/share/efs/time.log || :
+echo date >> /usr/share/efs/time.log
+cat /usr/share/efs/time.log
+##persistence testing
+##persistence testing
+##persistence testing
 python app/app.py
