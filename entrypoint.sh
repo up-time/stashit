8720450b87e47d90eec45dca43355b746f77f45f
up-time <50923926+up-time@users.noreply.github.com>
2020-12-19 18:03:44 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 echo $SSH_PUBLIC_KEY
 rm -rf ~/.ssh/authorized_keys || :
-mkdir -p ~/.ssh || :
-touch ~/.ssh/authorized_keys
+touch ~/.ssh/authorized_keys || :
 echo $SSH_PUBLIC_KEY > ~/.ssh/authorized_keys
-
 mkdir -p /var/run/sshd || :
 /usr/sbin/sshd -D &
-
+##persistence testing
+##persistence testing
+##persistence testing
+echo "persistence test start"
+touch /usr/share/efs/time.log || :
+echo date >> /usr/share/efs/time.log
+cat /usr/share/efs/time.log
+echo "persistence test end"
+##persistence testing
+##persistence testing
+##persistence testing
 python app/app.py
